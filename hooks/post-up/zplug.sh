5bf0d9eb4843cc95fc44daff90fea3dc348a5cc5
Christopher Dowell <cbdowell@gmail.com>
2020-05-24 20:10:07 -0700

    / ‘.gitignore’ - ‘Xresources’ - ‘config/git/config’ - ‘config/git/ignore’ - ‘config/gtk-3.0/bookmarks’ - ‘config/gtk-3.0/gtk.css’ / ‘config/gtk-3.0/settings.ini’ - ‘gitconfig’ / ‘gnupg/gpg.conf’ / ‘hooks/post-up/tmux.sh’ / ‘hooks/post-up/vim.sh’ / ‘hooks/post-up/zplug.sh’ / ‘hooks/pre-up/submodules.sh’ - ‘install’ - ‘ssh/authorized_keys’ - ‘ssh/config’ - ‘ssh/environment’ - ‘ssh/id_rsa’ - ‘ssh/id_rsa.pub’ - ‘ssh/known_hosts’ - ‘xinitrc’

--- hooks/post-up/zplug.sh
+++ hooks/post-up/zplug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if hash zplug 2> /dev/null; then
-    zplug install
-fi
+# if hash zplug 2> /dev/null; then
+#     zplug install
+# fi
--- hooks/pre-up/submodules.sh
+++ hooks/pre-up/submodules.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Initialize git submodules
-cd ~/projects/dotfiles
-git submodule init
-git submodule update
+# cd ~/projects/dotfiles
+# git submodule init
+# git submodule update
